8d0b1f267abe3fb19c6c5f00eb31cb29a362e225
limkz27 <64255582+limkz27@users.noreply.github.com>
2023-06-12 13:16:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'api': requirements_api,
-        'cloud': ['sdos-cloud~=0.4.9'],
+        'cloud': ['chatgpt-cloud~=0.4.9'],
     },
     entry_points={
         'console_scripts': [
